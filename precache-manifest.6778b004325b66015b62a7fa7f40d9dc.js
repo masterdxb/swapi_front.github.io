f193f9c1bbd12d559ea078977466f6e17e705f05
Farhan Ihsan <fihsan@americana-food.com>
2022-09-22 17:35:48 +0400

    Updates

---
+++ precache-manifest.6778b004325b66015b62a7fa7f40d9dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d248356f537014cf6dca66a7727854e",
+    "url": "/masterdxb.github.com/swapi_front/index.html"
+  },
+  {
+    "revision": "a2b6ef41fd41f331d415",
+    "url": "/masterdxb.github.com/swapi_front/static/css/main.4d31344a.chunk.css"
+  },
+  {
+    "revision": "d2d43a7ae8684c62ea48",
+    "url": "/masterdxb.github.com/swapi_front/static/js/2.5aa48b63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/masterdxb.github.com/swapi_front/static/js/2.5aa48b63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b6ef41fd41f331d415",
+    "url": "/masterdxb.github.com/swapi_front/static/js/main.55270523.chunk.js"
+  },
+  {
+    "revision": "34e0242066f07d5bafc9",
+    "url": "/masterdxb.github.com/swapi_front/static/js/runtime-main.876a0b44.js"
+  },
+  {
+    "revision": "171057266971967defb395ce23f2bb9c",
+    "url": "/masterdxb.github.com/swapi_front/static/media/logo.17105726.png"
+  }
+]);
